1d1e4dc68440184b712ad19720b4cda21a2851c8
Clement Boiteux <ezrabridger27@gmail.com>
2021-05-27 11:29:48 -0700

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ let opinions = ['I hate bananas.', 'Stinky cheese sucks.', 'C# is Java on steroi
 let farewells = ['Farewell, my friend!', ':wave:', 'Bye! Have a good day!', 'Cya', 'Ttyl'];
 let fortunes = ['You will be wealthy in the future.', 'You will end up like a bum.', 'You will be the next Elon Musk.', 'Good things will happen if you give to people who need.', 'You will score all 1\'s on your AP tests.', 'You will score all 5\'s on your AP tests.', 'You will be forgiven of your sins.'];
 let wisdoms = ['Confucius said, admitting that you don\'t know something is knowledge.'];
-let motivations = ['If you do your homework I\'ll buy you a family-sized cookie.', 'Dream big. Work hard. Win big.','You got this!!!', 'If you do your homework you are smart and motivated.', 'Study hard and you will achieve your dreams of becoming your favorite job.'];
+let motivations = ['If you do your homework I\'ll buy you a family-sized cookie.', 'Dream big. Work hard. Win big.','You got this!!!', 'If you do your homework you are smart and motivated.', 'Study hard and you will achieve your dreams of working in your favorite job.'];
 let roasts = ['You eat more than you live.', 'It seems that only your body is growing, not the brain...', 'Get a life.', 'You\'re using me because your family left you.', 'You spend a lot of time with me instead of getting straight A\'s.', 'Your mom is so dumb because she brought a spoon to the Super Bowl.', 'You should be spending more time with your friends than me.'];
 
 client.once('ready', () => {
@@ -35,12 +35,21 @@ client.on('message', message => {
         if (message.content.toLowerCase().indexOf('grind') >= 0) {
             message.channel.send('letsgooooooo!');
         } 
+        if (message.content.toLowerCase().indexOf('yeppy') >= 0) {
+            message.channel.send('Hello, my brother!');
+        }
+        if (message.content.toLowerCase().indexOf('clement') >= 0) {
+            message.channel.send('Bless the maker!');
+        }
+        if (message.content.indexOf('math sucks') >= 0 || message.content.indexOf('mathsucks') >= 0) {
+            client.commands.get('math sucks').execute(message, args);
+        }
     } else {
         if (command === 'brain') {
             client.commands.get('brain').execute(message, args);
         } else if (command === 'fact') {
             client.commands.get('fact').execute(message, args, facts);
-        } else if (command === 'hello' || command.toLowerCase() === 'hi') {
+        } else if (command === 'hello' || command === 'hi') {
             client.commands.get('hello').execute(message, args, greets);
         } else if (command === 'roast') {
             client.commands.get('roast').execute(message, args, roasts);
@@ -58,8 +67,6 @@ client.on('message', message => {
                 trimmed.push(comm);
             }
             message.channel.send(trimmed);
-        } else if (command === 'math sucks' || command === 'mathsucks') {
-            client.commands.get('math sucks').execute(message, args);
         } else if (command === 'opinion') {
             client.commands.get('opinion').execute(message, args, opinions);
         } else if (command === 'bye') {
@@ -69,13 +76,13 @@ client.on('message', message => {
         } else if (command === 'motivate') {
             client.commands.get('motivate').execute(message, args, motivations);
         } else if (command === 'wisdom') {
-            client.commands.get('wisdom').exeucte(message, args, wisdoms);
+            client.commands.get('wisdom').execute(message, args, wisdoms);
         } else if (command === 'meditate') {
             client.commands.get('meditate').execute(message, args);
+        } else if (command === 'bless') {
+
         }
     }
-    
-    
 })
 
 function answerQuestion(question) {
