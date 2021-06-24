5bc35cf0f8b8cfd4f49098b14606a60b4f298cb1
Clement Boiteux <67805925+iTakedown27@users.noreply.github.com>
2021-06-24 16:35:16 -0700

    Update mathsucks.js

--- commands/mathsucks.js
+++ commands/mathsucks.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: "math sucks",
-    description: "Fuck you",
+    description: "):",
     execute(message, args) {
         message.channel.send('https://tenor.com/view/christmas-tree-hit-crazy-random-angry-gif-19034195');
     }
