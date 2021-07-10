7d8b2ebd8202d5fd07a2ec3101f07cb0174bd41d
Clement Boiteux <ezrabridger27@gmail.com>
2021-07-10 13:24:04 -0700

    .

--- commands/meditate.js
+++ commands/meditate.js
@@ -2,25 +2,8 @@ module.exports = {
     name: "meditate",
     description: "Performs Clementian Meditation",
     execute(message, args) {
-        let steps = ['The Clementian Medidation Principles', 
-        '1. Don\'t hate. Only forgive.'
-        ,'2. Always think about what you are doing in life that helps other people.'
-        ,'3. NEVER THINK NEGATIVELY.'
-        ,'4. You must calm down.'
-        ,'5. Believe.'
-        ,'6. Be determined.'
-        ,'7. Be courageous.'
-        ,'8. Be thankful.'
-        ,'9. Be understanding.'
-        ,'10. Be the right person.'];
-        var mins = Date.now();
-        message.channel.send('We shall begin the meditation process. Breathe in. Close your eyes. Think. Every principle practice lasts for 2 minutes. Begin.');
-        for (let i = 0; i < steps.length && mins < 120000; i++) {
-            message.channel.send(steps[i]);
-            if (mins === 119999) {
-                message.channel.send('Good. Now, moving on to the next principle.');
-            }
-        }
-        message.channel.send('The meditation process has ended. You may be free. Remember to stay calm, think wisely, and don\'t harm others. Good luck on your journey, my friend.');
+        let steps = ['The Clementian Meditation Principles', '1. Don\'t hate. Only forgive.','2. Always think about what you are doing in life that helps other people.','3. NEVER THINK NEGATIVELY.','4. You must calm down.','5. Believe.','6. Be determined.','7. Be courageous.','8. Be thankful.','9. Be understanding.','10. Be the right person.'];
+        message.channel.send(steps);
+        message.channel.send("Breathe in and breathe out, perform each one for 1 minute graciously.");
     }
 }
--- index.js
+++ index.js
@@ -13,23 +13,23 @@ for (const file of commandFiles) {
 }
 
 const prefix = 'nooby ';
-let facts = ['In the horizontal direction, projectiles do not have acceleration and their velocity remains constant throughout.', 'The Law of Conservation of Matter applies to all fields in science. "Nothing is lost, nothing is gained, everything is transformed." - Antoine Lavoisier', '6 times 9 plus 6 plus 9 equals 69!', 'The infinite sum of a geometric series whose common ratio is less than the absolute value of 1 eventually converges towards a value. Formula: a1/1-r', 'A solution is not a solution if it isn\'t homogenous.', '1 electronVolt = 1.6 * 10^-19 J', 'c = lambda * nu (Speed of light = wavelength * frequency)', 'e = h * nu (Energy = Planck\'s constant 6.626 * 10^-34 J s * frequency)', 'An electrolyte is an ionic compound that forms ions upon dissolving and conducts electricity. When connected with electrodes, they process electricity to power the electronic system connected to it.'];
-let greets = ['Howdy!', 'Hi!', 'Hello!', 'Sup!', 'Greetings'];
-let fails = ['https://tenor.com/view/slick-fall-ouch-fail-oops-gif-4402351'];
-let opinions = ['I hate bananas.', 'Stinky cheese sucks.', 'C# is Java on steroids.', 'Taiwan is not part of China.', 'Math is fun!', 'Art sucks.', 'Dawnold Jay Chrump is the worst man in history.', 'Java is a good coding language to start with.'];
-let farewells = ['Farewell, my friend!', ':wave:', 'Bye! Have a good day!', 'Cya', 'Ttyl'];
-let fortunes = ['You will be wealthy in the future.', 'You will end up like a bum.', 'You will be the next Elon Musk.', 'Good things will happen if you give to people who need.', 'You will score all 1\'s on your AP tests.', 'You will score all 5\'s on your AP tests.', 'You will be forgiven of your sins.'];
-let wisdoms = ['Confucius said, admitting that you don\'t know something is knowledge.'];
-let motivations = ['If you do your homework I\'ll buy you a family-sized cookie.', 'Dream big. Work hard. Win big.','You got this!!!', 'If you do your homework you are smart and motivated.', 'Study hard and you will achieve your dreams of working in your favorite job.'];
-let roasts = ['You eat more than you live.', 'It seems that only your body is growing, not the brain...', 'Get a life.', 'You\'re using me because your family left you.', 'You spend a lot of time with me instead of getting straight A\'s.', 'Your mom is so dumb because she brought a spoon to the Super Bowl.'];
-let confessions = ['I peed on the classroom carpet in 1st grade.', 'I beat up a first grader in kindergarten.', 'I have destroyed tons of ceramics in my life.'];
-let tips = ['Study hard! It will land you a good job.', 'Obey the laws.', 'Control your temper.', 'Be respectful.', 'Don\'t lie about important matters.', 'Always learn new things every day.'];
-let stuffToDo = ['Read a book.', 'Learn more about a subject.', 'Go play a game or 2 of any video game!', 'Go out for a walk!', 'Feed your pet.', 'Do your homework.', 'Do a hobby.', 'Learn a new skill.', 'Sleep.', 'Briefly check social media.', 'Do some chores.', 'Eat a snack.', 'Listen to some music.', 'Complete your Driver\'s Ed course.', 'Watch YouTube.', 'Get a LinkedIn profile.'];
+var facts = ['In the horizontal direction, projectiles do not have acceleration and their velocity remains constant throughout.', 'The Law of Conservation of Matter applies to all fields in science. "Nothing is lost, nothing is gained, everything is transformed." - Antoine Lavoisier', '6 times 9 plus 6 plus 9 equals 69!', 'The infinite sum of a geometric series whose common ratio is less than the absolute value of 1 eventually converges towards a value. Formula: a1/1-r', 'A solution is not a solution if it isn\'t homogenous.', '1 electronVolt = 1.6 * 10^-19 J', 'c = lambda * nu (Speed of light = wavelength * frequency)', 'e = h * nu (Energy = Planck\'s constant 6.626 * 10^-34 J s * frequency)', 'An electrolyte is an ionic compound that forms ions upon dissolving and conducts electricity. When connected with electrodes, they process electricity to power the electronic system connected to it.'];
+var greets = ['Howdy!', 'Hi!', 'Hello!', 'Sup!', 'Greetings'];
+var fails = ['https://tenor.com/view/slick-fall-ouch-fail-oops-gif-4402351'];
+var opinions = ['I hate bananas.', 'Stinky cheese sucks.', 'C# is Java on steroids.', 'Taiwan is not part of China.', 'Math is fun!', 'Art sucks.', 'Dawnold Jay Chrump is the worst man in history.', 'Java is a good coding language to start with.'];
+var farewells = ['Farewell, my friend!', ':wave:', 'Bye! Have a good day!', 'Cya', 'Ttyl'];
+var fortunes = ['You will be wealthy in the future.', 'You will end up like a bum.', 'You will be the next Elon Musk.', 'Good things will happen if you give to people who need.', 'You will score all 1\'s on your AP tests.', 'You will score all 5\'s on your AP tests.', 'You will be forgiven of your sins.'];
+var wisdoms = ['Confucius said, admitting that you don\'t know something is knowledge.'];
+var motivations = ['If you do your homework I\'ll buy you a family-sized cookie.', 'Dream big. Work hard. Win big.','You got this!!!', 'If you do your homework you are smart and motivated.', 'Study hard and you will achieve your dreams of working in your favorite job.'];
+var roasts = ['You eat more than you live.', 'It seems that only your body is growing, not the brain...', 'Get a life.', 'You\'re using me because your family left you.', 'You spend a lot of time with me instead of getting straight A\'s.', 'Your mom is so dumb because she brought a spoon to the Super Bowl.'];
+var confessions = ['I eat ice cream like a 5-year-old.','I peed on the classroom carpet in 1st grade.', 'I beat up a first grader in kindergarten.', 'I have destroyed tons of ceramics in my life.'];
+var tips = ['Study hard! It will land you a good job.', 'Obey the laws.', 'Control your temper.', 'Be respectful.', 'Don\'t lie about important matters.', 'Always learn new things every day.'];
+var stuffToDo = ['Read a book.', 'Learn more about a subject.', 'Go play a game or 2 of any video game!', 'Go out for a walk!', 'Feed your pet.', 'Do your homework.', 'Do a hobby.', 'Learn a new skill.', 'Sleep.', 'Briefly check social media.', 'Do some chores.', 'Eat a snack.', 'Listen to some music.', 'Complete your Driver\'s Ed course.', 'Watch YouTube.', 'Get a LinkedIn profile.'];
 
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
     console.log('Hello hello!');
-})
+});
 
 client.on('message', message => {
     const args = message.content.slice(prefix.length).split(/ + /);
@@ -38,9 +38,6 @@ client.on('message', message => {
     if (!message.content.toLowerCase().startsWith(prefix)) {
         if (message.content.toLowerCase().indexOf('grind') >= 0) {
             message.channel.send('letsgooooooo!');
-        } 
-        if (message.content.toLowerCase().indexOf('yeppy') >= 0) {
-            message.channel.send('Hello, my brother!');
         }
         if (message.content.toLowerCase().indexOf('clement') >= 0) {
             message.channel.send('Bless the maker!');
@@ -103,7 +100,6 @@ client.on('message', message => {
             client.commands.get('tip').execute(message, args, tips);
         }
     }
-})
+});
 
 client.login(config.token);
-// Last line
